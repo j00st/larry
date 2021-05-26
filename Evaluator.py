d96d0bb8e65bac51275a6f79bd6f51cd8521d3a3
BuildTools <unconfigured@null.spigotmc.org>
2021-05-27 00:48:27 +0200

    Parser and Evaluator refactored to full-functional programming style

--- Evaluator.py
+++ Evaluator.py
@@ -5,7 +5,7 @@ from functools import reduce
 class Evaluator(object):
     # __init__ :: None -> None
     def __init__(self) -> None:
-        self.globalmemory: dict = {}
+        pass
 
     # __str__ :: None -> String
     def __str__(self) -> str:
@@ -16,13 +16,13 @@ class Evaluator(object):
         return self.__str__()
 
     # evaluate :: Node -> Int -> None
-    def evaluate(self, nodes: Node, pos: int = 0) -> None:
+    def evaluate(self, nodes: Node, globalmemory: dict = {}, pos: int = 0) -> None:
         node: Node = nodes[pos]
-        node(self.globalmemory)
+        node(globalmemory)
         if pos+1 >= len(nodes):
-            memory_string = reduce(lambda x, y: str(x) + '\n' + str(y)+ ' : ' + str(self.globalmemory[y]), self.globalmemory, '\n\nGlobal memory:')
+            memory_string = reduce(lambda x, y: str(x) + '\n' + str(y)+ ' : ' + str(globalmemory[y]), globalmemory, '\n\nGlobal memory:')
             logfile = open("larrylog.txt","a")
             logfile.write(memory_string)
             logfile.close()
             return
-        return self.evaluate(nodes, pos+1)
+        return self.evaluate(nodes, globalmemory, pos+1)
